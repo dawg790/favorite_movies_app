1b4ce59c8a821061fc9adbf441add91eb8d0e7fd
Nick Woodland <nick_woodland@hotmail.com>
2015-06-15 11:30:38 -0600

    Update Styling in fresh_tomates.py for movie items

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -6,7 +6,7 @@ import re
 main_page_head = '''
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <title>Nick's Movie Page</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -52,6 +52,9 @@ main_page_head = '''
             left: 0;
             top: 0;
             background-color: white;
+        }
+        .movie-cast {
+            color: #1080dd;
         }
     </style>
     <script type="text/javascript" charset="utf-8">
@@ -99,13 +102,13 @@ main_page_content = '''
         </div>
       </div>
     </div>
-    
+
     <!-- Main Page Content -->
     <div class="container">
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Nick's Movie Page</a>
           </div>
         </div>
       </div>
@@ -122,6 +125,8 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
+    <h5>{storyline}</h5>
+    <p class="movie-cast"><em>{cast}</em></p>
 </div>
 '''
 
@@ -137,6 +142,8 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
+            storyline = movie.storyline,
+            cast = movie.cast,
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id
         )
