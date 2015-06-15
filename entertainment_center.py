7bad13cee6f0acd17c8e9f4083e15f82d583239a
Nick Woodland <nick_woodland@hotmail.com>
2015-06-15 11:32:04 -0600

    Add cast members to each movie instance

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,24 +1,47 @@
 import fresh_tomatoes
 import media
 
-toy_story = media.Movie("Toy Story",
-                        "A movie about toys coming to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+star_wars = media.Movie("Star Wars",
+                        "The Force Awakens - Episode VII",
+                        "https://upload.wikimedia.org/wikipedia/en/7/73/Forceawakenspromo.jpg",
+                        "https://www.youtube.com/watch?v=OprvFVBGtEc",
+                        "Harrison Ford, Mark Hamill, Carrie Fisher")
 
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+interstellar = media.Movie("Interstellar",
+                            "Humans search for an inhabitable planet",
+                            "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+                            "https://www.youtube.com/watch?v=lRTTCrdzKAo",
+                            "Matthew McConaughey, Michael Caine")
 
 skyfall = media.Movie("Skyfall",
                       "Ian Fleming's 007 Bond adventure",
                       "http://www.impawards.com/2012/posters/skyfall.jpg",
-                      "www.youtube.com/watch?v=6kw1UVovByw")
+                      "www.youtube.com/watch?v=6kw1UVovByw",
+                      "Daniel Craig")
 
-## movies = [toy_story, avatar, skyfall]
-## fresh_tomatoes.open_movies_page(movies)
+swingers = media.Movie("Swingers",
+                        "Cocktails first, questions later.",
+                        "https://upload.wikimedia.org/wikipedia/en/9/91/Swingers_ver2.jpg",
+                        "https://www.youtube.com/watch?v=nWCct8XbQD0",
+                        "Vince Vaughn, Jon Favreau")
 
-print(media.Movie.__doc__)
-print(media.Movie.__name__)
-print(media.Movie.__module__)
+braveheart = media.Movie("Braveheart",
+                         "William Wallace fights for Scottish Independence",
+                         "https://upload.wikimedia.org/wikipedia/en/5/55/Braveheart_imp.jpg",
+                         "https://www.youtube.com/watch?v=wj0I8xVTV18",
+                         "Mel Gibson")
+
+jaws = media.Movie("Jaws",
+                    "A shark terrorizes a summer community",
+                    "https://upload.wikimedia.org/wikipedia/en/e/eb/JAWS_Movie_poster.jpg",
+                    "https://www.youtube.com/watch?v=U1fu_sA7XhE",
+                    "Richard Dreyfus, Roy Schneider, Robert Shaw")
+
+
+
+movies = [star_wars, interstellar, skyfall, swingers, braveheart, jaws]
+fresh_tomatoes.open_movies_page(movies)
+
+#print(media.Movie.__doc__)
+#print(media.Movie.__name__)
+#print(media.Movie.__module__)
