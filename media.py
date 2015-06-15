84470485846bf6598f1d977d4b08af300b8781fb
Nick Woodland <nick_woodland@hotmail.com>
2015-06-15 11:43:26 -0600

    Update comments for media file

--- media.py
+++ media.py
@@ -1,10 +1,11 @@
 import webbrowser
 
 class Movie():
-    ## This below can be accessed via the doc method on Classes (Media.__doc__)
+    # This line below can be accessed via the doc method on Classes (Media.__doc__)
     """ This class provides a way to store movie related information """
 
-    ## This is a class variable which is also a constant
+    # This is a class variable which is also a constant
+    # TODO: use these on the movie page to show the ratings for each movie.
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, cast):
