e240b22a0f7d4970b9e7c1b5d729f86024c785c1
Nick Woodland <nick_woodland@hotmail.com>
2015-06-15 22:32:44 -0600

    Fixed extra double quotes in poster image url parameters

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,15 +3,15 @@ import media
 
 # My Movie Instances below, instantiated from the Media Class
 star_wars = media.Movie("Star Wars", "The Force Awakens - Episode VII",
-                        '"https://upload.wikimedia.org/wikipedia/en/7/ '
-                        ' 73/Forceawakenspromo.jpg"',
+                        'https://upload.wikimedia.org/wikipedia/en/7/'
+                        '73/Forceawakenspromo.jpg',
                         "https://www.youtube.com/watch?v=OprvFVBGtEc",
                         "Harrison Ford, Mark Hamill, Carrie Fisher")
 
 interstellar = media.Movie("Interstellar",
                            "Humans search for an inhabitable planet",
-                           '"https://upload.wikimedia.org/wikipedia/ '
-                           'en/b/bc/Interstellar_film_poster.jpg"',
+                           'https://upload.wikimedia.org/wikipedia/'
+                           'en/b/bc/Interstellar_film_poster.jpg',
                            "https://www.youtube.com/watch?v=lRTTCrdzKAo",
                            "Matthew McConaughey, Michael Caine")
 
@@ -23,22 +23,22 @@ skyfall = media.Movie("Skyfall",
 
 swingers = media.Movie("Swingers",
                        "Cocktails first, questions later.",
-                       '"https://upload.wikimedia.org/wikipedia/en '
-                       '/9/91/Swingers_ver2.jpg"',
+                       'https://upload.wikimedia.org/wikipedia/en'
+                       '/9/91/Swingers_ver2.jpg',
                        "https://www.youtube.com/watch?v=nWCct8XbQD0",
                        "Vince Vaughn, Jon Favreau")
 
 braveheart = media.Movie("Braveheart",
                          "William Wallace fights for Scottish Independence",
-                         '"https://upload.wikimedia.org/wikipedia/en '
-                         '/5/55/Braveheart_imp.jpg"',
+                         'https://upload.wikimedia.org/wikipedia/en'
+                         '/5/55/Braveheart_imp.jpg',
                          "https://www.youtube.com/watch?v=wj0I8xVTV18",
                          "Mel Gibson")
 
 jaws = media.Movie("Jaws",
                    "A shark terrorizes a summer community",
-                   '"https://upload.wikimedia.org/wikipedia/en '
-                   '/e/eb/JAWS_Movie_poster.jpg"',
+                   'https://upload.wikimedia.org/wikipedia/en'
+                   '/e/eb/JAWS_Movie_poster.jpg',
                    "https://www.youtube.com/watch?v=U1fu_sA7XhE",
                    "Richard Dreyfus, Roy Schneider, Robert Shaw")
 
